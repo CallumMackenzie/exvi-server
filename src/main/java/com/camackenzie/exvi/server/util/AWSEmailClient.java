4c11ff5dae3d65fb055d3b9663e1c5ab6ee5cf46
Callum Mackenzie <Alexxander1611@gmail.com>
2022-01-20 16:07:34 -0700

    Initial design commit.

---
+++ src/main/java/com/camackenzie/exvi/server/util/AWSEmailClient.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.camackenzie.exvi.server.util;
+
+/**
+ *
+ * @author callum
+ */
+public class AWSEmailClient implements EmailClient {
+
+    @Override
+    public void sendEmail(String sender, String recipient, String subject, String htmlBody, String plainTextBody) {
+    }
+    
+}
---
+++ src/main/java/com/camackenzie/exvi/server/util/EmailClient.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.camackenzie.exvi.server.util;
+
+/**
+ *
+ * @author callum
+ */
+public interface EmailClient {
+
+    public void sendEmail(String sender, String recipient, String subject, String htmlBody, String plainTextBody);
+}
