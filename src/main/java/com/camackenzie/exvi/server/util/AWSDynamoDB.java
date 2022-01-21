8ac79b58ebe52493588d301796efa13ed7e6af8e
Callum Mackenzie <Alexxander1611@gmail.com>
2022-01-21 10:11:04 -0700

    Updated AWS utils with basic functionality.

--- src/main/java/com/camackenzie/exvi/server/util/AWSDynamoDB.java
+++ src/main/java/com/camackenzie/exvi/server/util/AWSDynamoDB.java
@@ -5,10 +5,55 @@
  */
 package com.camackenzie.exvi.server.util;
 
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.document.DynamoDB;
+import com.amazonaws.services.dynamodbv2.document.Table;
+import java.util.HashMap;
+
 /**
  *
  * @author callum
  */
 public class AWSDynamoDB {
-    
+
+    private final AmazonDynamoDB awsDynamoDB;
+    private final DynamoDB docClient;
+    private final HashMap<String, Table> tableNameMap;
+
+    public AWSDynamoDB() {
+        this.awsDynamoDB = AmazonDynamoDBClientBuilder
+                .standard()
+                .withRegion(Regions.US_EAST_2)
+                .build();
+        this.docClient = new DynamoDB(this.awsDynamoDB);
+        this.tableNameMap = new HashMap<>();
+    }
+
+    public HashMap<String, Table> getTableNameMap() {
+        return this.tableNameMap;
+    }
+
+    public Table cacheTable(String name) {
+        Table t = this.getTable(name);
+        this.tableNameMap.put(name, t);
+        return t;
+    }
+
+    public Table getTable(String name) {
+        if (this.tableNameMap.containsKey(name)) {
+            return this.tableNameMap.get(name);
+        }
+        return this.docClient.getTable(name);
+    }
+
+    public DynamoDB getDocClient() {
+        return this.docClient;
+    }
+
+    public AmazonDynamoDB getAwsDynamoDB() {
+        return this.awsDynamoDB;
+    }
+
 }
