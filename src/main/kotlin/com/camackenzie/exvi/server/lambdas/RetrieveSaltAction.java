d73ac0a3f0198c1ffd635754b7a85f5351b4d1d5
Callum Mackenzie <Alexxander1611@gmail.com>
2022-02-17 12:23:33 -0700

    Add .get for encoded string cache

--- src/main/kotlin/com/camackenzie/exvi/server/lambdas/RetrieveSaltAction.java
+++ src/main/kotlin/com/camackenzie/exvi/server/lambdas/RetrieveSaltAction.java
@@ -14,6 +14,7 @@ import com.camackenzie.exvi.core.api.RetrieveSaltRequest;
 import com.camackenzie.exvi.core.util.CryptographyUtils;
 import com.camackenzie.exvi.server.util.AWSDynamoDB;
 import com.camackenzie.exvi.server.util.RequestException;
+
 import java.nio.charset.StandardCharsets;
 
 /**
@@ -30,14 +31,14 @@ public class RetrieveSaltAction
     public AccountSaltResult handleBodyRequest(RetrieveSaltRequest in, Context context) {
         AWSDynamoDB database = new AWSDynamoDB();
         Table accountTable = database.cacheTable("exvi-user-login");
-        Item item = accountTable.getItem("username", in.getUsername());
+        Item item = accountTable.getItem("username", in.getUsername().get());
 
         if (item == null) {
             throw new RequestException(400, "User not found");
         } else if (!item.hasAttribute("salt")) {
             throw new RequestException(400, "No valid user login entry");
         } else {
-            return new AccountSaltResult("Success",
+            return new AccountSaltResult(0, "Success",
                     CryptographyUtils.bytesToBase64String(item.getString("salt")
                             .getBytes(StandardCharsets.UTF_8)));
         }
