d1cf5a4540a58dfcab7f3c9321edb11f49ff16cc
callum <alexxander1611@gmail.com>
2022-05-30 15:14:47 -0600

    Ensure private workouts are not returned for friends

--- src/main/kotlin/com/camackenzie/exvi/server/lambdas/GetFriendWorkoutsAction.java
+++ src/main/kotlin/com/camackenzie/exvi/server/lambdas/GetFriendWorkoutsAction.java
@@ -11,6 +11,7 @@ import com.camackenzie.exvi.server.util.RequestBodyHandler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 public class GetFriendWorkoutsAction implements LambdaAction<GetFriendWorkouts, RemoteWorkoutResponse> {
     @Override
@@ -22,6 +23,8 @@ public class GetFriendWorkoutsAction implements LambdaAction<GetFriendWorkouts, 
         if (!user.isFriendsWith(friend)) throw new ApiException(400, "User is not a friend");
 
         var remoteWorkouts = Arrays.stream(friend.getWorkouts())
+                .map(it -> it.getPublic() ? it : null)
+                .filter(Objects::nonNull)
                 .map(it -> new RemoteWorkout(new EncodedStringCache(it.getName()),
                         it.getId(),
                         req.getFriend()))
